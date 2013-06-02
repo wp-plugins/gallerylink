2c729732666284207dce43ffe43847b7b2adcd3a
Katsushi Kawamori <Katsushi Kawamori@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-06-02 10:54:29 +0000

    Change readme.txt version 1.0.4
    git-svn-id: https://plugins.svn.wordpress.org/gallerylink@721613 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/gallerylink.php
+++ trunk/gallerylink.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: GalleryLink
 Plugin URI: http://wordpress.org/plugins/gallerylink/
-Version: 1.0.3
+Version: 1.0.4
 Description: Output as a gallery by find the file extension and directory specified.
 Author: Katsushi Kawamori
 Author URI: http://gallerylink.nyanko.org/
