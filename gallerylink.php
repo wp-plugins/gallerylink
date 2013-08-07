68e7781136a007e950401f96e77a572ec888eac0
Katsushi Kawamori <Katsushi Kawamori@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-07 15:20:34 +0000

    Change /languages version 2.14
    git-svn-id: https://plugins.svn.wordpress.org/gallerylink/trunk@753032 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- gallerylink.php
+++ gallerylink.php
@@ -2,7 +2,7 @@
 /*
 Plugin Name: GalleryLink
 Plugin URI: http://wordpress.org/plugins/gallerylink/
-Version: 2.13
+Version: 2.14
 Description: Output as a gallery by find the file extension and directory specified.
 Author: Katsushi Kawamori
 Author URI: http://gallerylink.nyanko.org/
